f4a8ae28105f915ad20fecc3ee0e04c2470d7651
Chandra Delon <34028244+vinix997@users.noreply.github.com>
2019-12-10 16:43:22 +0700

    Assets implement

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
 $(document).ready(function(){
     $('.menu-toggle').click(function(){
       $('nav').toggleClass('active');
